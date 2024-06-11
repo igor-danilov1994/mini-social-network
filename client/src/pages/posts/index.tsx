f2e426e4e0f8d9cd86450afe653ab16077ea2feb
igor <igor.danilov1824@gmail.com>
2024-06-11 10:52:38 +0300

    create Posts component on client part

--- client/src/pages/posts/index.tsx
+++ client/src/pages/posts/index.tsx
@@ -1,5 +1,38 @@
-import { FC, memo } from "react"
+import type { FC } from "react"
+import { memo } from "react"
+import { useGetAllPostsQuery } from "../../app/services/postApi"
+import { CreatePost } from "../../components/create-post"
+import { CustomCard } from "../../components/card"
 
 export const Posts: FC = memo(props => {
-  return <div>Posts</div>
+  const { data } = useGetAllPostsQuery()
+
+  return (
+    <>
+      <div className="mb-10 w-full">
+        <CreatePost />
+      </div>
+      {data && data.length > 0 && (
+        <>
+          {data.map(post => {
+            return (
+              <CustomCard
+                key={post.id}
+                cardFor={"post"}
+                author={post.author}
+                authorId={post.authorId}
+                likes={post.likes}
+                comments={post.comments}
+                likedByUser={post.likedByUser}
+                createdAt={post.createdAt}
+                updatedAt={post.updatedAt}
+                id={post.id}
+                content={post.content}
+              />
+            )
+          })}
+        </>
+      )}
+    </>
+  )
 })

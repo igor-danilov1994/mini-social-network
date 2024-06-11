c4b097f893e2be0ef85c79dc359647d4b64a14f5
igor <igor.danilov1824@gmail.com>
2024-06-11 10:52:29 +0300

    create Followers component on client part

--- client/src/pages/followers/index.tsx
+++ client/src/pages/followers/index.tsx
@@ -1,5 +1,36 @@
-import { FC, memo } from "react"
+import type { FC } from "react"
+import { memo } from "react"
+import { Link } from "react-router-dom"
+import { Card, CardBody } from "@nextui-org/react"
+
+import { useCurrentQuery } from "../../app/services/userApi"
+import { CustomUser } from "../../components/user"
 
 export const Followers: FC = memo(props => {
-  return <div>Followers</div>
+  const { data } = useCurrentQuery()
+
+  if (!data?.followers?.length) {
+    return <h2>Подписчиков нет!</h2>
+  }
+
+  return (
+    <div>
+      <h1 className="mb-5">Подписчиков: {data?.followers?.length}</h1>
+      {data.followers.map(user => {
+        return (
+          <Card>
+            <CardBody>
+              <Link to={`/users/${user.followingId}`} key={user.followingId}>
+                <CustomUser
+                  avatarUrl={user.followerId.avatarUrl}
+                  name={user.followerId.name}
+                  description={user.followerId.email}
+                />
+              </Link>
+            </CardBody>
+          </Card>
+        )
+      })}
+    </div>
+  )
 })
